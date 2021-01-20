6836aedb2af7d5ea627f8cf71f328d630281889c
Artyom Ilyin <artemilyin@coherentsolutions.com>
2021-01-20 11:37:03 +0300

    added thank you page, thank you page component, home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { ProductsComponent } from './components/products/products.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ReactiveFormsModule } from "@angular/forms";
 import { AddProductComponent } from './components/add-product/add-product.component';
+import { RouterModule } from "@angular/router";
+import { ThankYouPageComponent } from './components/thank-you-page/thank-you-page.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +18,24 @@ import { AddProductComponent } from './components/add-product/add-product.compon
     CartComponent,
     ProductsComponent,
     HeaderComponent,
-    AddProductComponent
+    AddProductComponent,
+    ThankYouPageComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'thank-you',
+        component: ThankYouPageComponent
+      },
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
